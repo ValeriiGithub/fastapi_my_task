5564512b24551ebe8a6b20957f43df120acde4d9
Valerii <valeri.github@gmail.com>
2024-02-14 17:26:57 +0300

    Change to get tasks

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from schemas.task import Task
 
 
 app = FastAPI()
 
 
-@app.get("/home")
-async def home():
-    return {"message": "Hello World"}
+@app.get("/tasks")
+async def get_tasks():
+    task = Task(name="Запиши эту задачу")
+    return {"data": task}
