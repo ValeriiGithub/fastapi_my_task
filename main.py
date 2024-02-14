278a5a8c4c0c91ee98665114f4b15e63678f037b
Valerii <valeri.github@gmail.com>
2024-02-14 17:43:07 +0300

    Import Annotated to python 3.8.10

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+# from typing import Annotated    # с версии python 3.9
+from typing_extensions import Annotated
+
+from fastapi import FastAPI, Depends
 from schemas.task import STask, STaskAdd
 
 
@@ -6,11 +9,11 @@ app = FastAPI()
 
 tasks = []
 @app.post("/tasks")
-async def create_task(task: STaskAdd,
+async def create_task(
+        task: Annotated[STaskAdd, Depends()],
                       ):
     tasks.append(task)
-    return {"ok": True,
-            "task": task}
+    return {"ok": True}
 
 
 # @app.get("/tasks")
