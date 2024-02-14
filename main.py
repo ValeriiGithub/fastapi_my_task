eb947d489dc1785e4a039f3ea8fab4ab901f6231
Valerii <valeri.github@gmail.com>
2024-02-14 17:34:57 +0300

    Add create_task

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
-from schemas.task import STask
+from schemas.task import STask, STaskAdd
 
 
 app = FastAPI()
 
+tasks = []
+@app.post("/tasks")
+async def create_task(task: STaskAdd,
+                      ):
+    tasks.append(task)
+    return {"ok": True,
+            "task": task}
 
-class
 
-
-@app.get("/tasks")
-async def get_tasks():
-    task = STask(name="Запиши эту задачу")
-    return {"data": task}
+# @app.get("/tasks")
+# async def get_tasks():
+#     task = STask(name="Запиши эту задачу")
+#     return {"data": task}
