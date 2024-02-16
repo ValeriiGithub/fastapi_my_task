487cf4060311156df1862e0cee40bc913ffd57d2
Valerii <valeri.github@gmail.com>
2024-02-16 17:05:50 +0300

    Create logic how write to DB

--- repository.py
+++ repository.py
@@ -1,10 +1,22 @@
-
+from database import new_session, TaskOrm
+from schemas.task import STaskAdd
 
 
 class TaskRepository:
     @classmethod
-    async def add_one(cls):
-        ...
+    async def add_one(cls, data: STaskAdd) -> int:
+        async with new_session() as session:
+            task_dict = data.model_dump()       # приводим data к виду dict
+
+            task = TaskOrm(**task_dict)
+
+            # Блок добавления task в БД
+            session.add(task)                   # эта операция синхронная т.к. к БД еще не обращаемся
+            await session.flush()               # еще не завершает транзакцию (добавление строки), отправит изменения
+                                                # в БД и получит id при автоинкременте
+                                                # т.о. поле id будет зафиксировано, и мы сможем его вернуть
+            await session.commit()              # сессия отправляется в БД со сделанными изменениями через коммит
+            return task.id
 
     @classmethod
     async def get_all(cls):
